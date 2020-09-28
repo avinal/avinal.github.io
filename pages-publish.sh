c310a16f814870f07d4d13c4798a478901b9e347
avinal <185067@nith.ac.in>
2020-09-28 17:25:41 +0530

    small test

--- pages-publish.sh
+++ pages-publish.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 # install tools
-apt-get update && apt-get install -y git make python3 python3-pip 
+sudo apt-get update && apt-get install -y git make python3 python3-pip 
 
 # setup github config
 git config user.email "185067@nith.ac.in"
 git config user.name "avinal"
+echo "git config added"
 
 # install dependencies
 pip3 install -r requirements.txt
