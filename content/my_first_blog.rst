a4239c6f28454566a2456a9357ada92891f61b21
avinal <185067@nith.ac.in>
2020-09-28 19:52:28 +0530

    a lot of changes

--- content/my_first_blog.rst
+++ content/my_first_blog.rst
@@ -10,7 +10,12 @@ If in the **Twilight** of dreams we should meet once more, we shell talk again t
 shall sing to you a lullaby till you sleep again to meet me in the **Twilight** of another dream.
 
 .. figure:: /images/interstellar_twilight.jpg
-    :height: 500
+    :height: 800
+    :align: center
     :alt: The sunset
+
+    This is not what I am talking about :)
+
+
 
    
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 
 AUTHOR = 'Avinal'
 SITENAME = 'The Interstellar Twilight'
-SITEURL = ''
+SITEURL = 'https://avinal.github.io'
 THEME = 'alchemy'
 PATH = 'content'
 SITEIMAGE = '/images/profile.jpg width=150 height=150'
@@ -18,11 +18,37 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('GitHub', 'https://github.com/avinal'),
-         ('LinkedIn', ''),)
 
+# URL Flexibility
+ARTICLE_URL = 'posts/{category}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{category}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+
+# static content setting
+STATIC_PATHS = ['extras', 'images']
+EXTRA_PATH_METADATA = {
+    'extras/android-chrome-192x192.png': {'path': 'android-chrome-192x192.png'},
+    'extras/android-chrome-512x512.png': {'path': 'android-chrome-512x512.png'},
+    'extras/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'extras/browserconfig.xml': {'path': 'browserconfig.xml'},
+    'extras/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+    'extras/favicon-32x32.png': {'path': 'favicon-32x32.png'},
+    'extras/favicon.ico': {'path': 'favicon.ico'},
+    'extras/site.webmanifest': {'path': 'site.webmanifest'},
+    'extras/mstile-150x150.png': {'path': 'mstile-150x150.png'},
+    'extras/mstile-70x70.png': {'path': 'mstile-70x70.png'},
+    'extras/mstile-310x310.png': {'path': 'mstile-310x310.png'},
+    'extras/mstile-144x144.png': {'path': 'mstile-144x144.png'},
+    'extras/safari-pinned-tab.svg': {'path': 'safari-pinned-tab.svg'},
+}
+
+# icons
+ICONS = [
+    ('github', "https://github.com/avinal"),
+    ('gitlab', 'https://code.videolan.org/avinal'),
+    ('linkedin', 'https://www.linkedin.com/in/avinal/'),
+    ('instagram', 'https://www.instagram.com/amaranthine.sole/'),
+]
 
 DEFAULT_PAGINATION = 10
-
-
