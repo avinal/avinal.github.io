ef5563f44927e01128692bedd610ac8abb4963ec
avinal <185067@nith.ac.in>
2021-02-08 18:05:18 +0530

    test domain

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://blog.avinal.space'
+SITEURL = 'https://www.avinal.space'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
