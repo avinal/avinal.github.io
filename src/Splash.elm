e46f0dfbbeef86afc52d36626a1d6dcff2fd55c6
Avinal Kumar <avinal.xlvii@gmail.com>
2022-09-15 21:27:32 +0530

    fix styling issue
    Signed-off-by: Avinal Kumar <avinal.xlvii@gmail.com>

--- src/Splash.elm
+++ src/Splash.elm
@@ -24,13 +24,10 @@ view model =
                 []
             , div [ class "foo-support__message" ] [ model.support_message ]
             , div [ class "foo-error__message" ] [ model.error_message ]
-            , div [ class "foo-support__message" ]
+            , withSpacing (div [ class "foo-support__message" ])
                 [ a [ href "https://avinal.space/pages/about-me" ] [ text "About me" ]
-                , text "&nbsp;&nbsp;&nbsp;"
                 , a [ href "https://avinal.space/posts" ] [ text "Blog" ]
-                , text "&nbsp;&nbsp;&nbsp;"
                 , a [ href "https://avinal.space/pages/projects" ] [ text "Projects" ]
-                , text "&nbsp;&nbsp;&nbsp;"
                 , a [ href "https://gsoc.avinal.space" ] [ text "GSoC" ]
                 ]
             ]
@@ -41,16 +38,21 @@ type Msg
     = Nothing
 
 
+withSpacing : (List (Html msg) -> Html msg) -> List (Html msg) -> Html msg
+withSpacing element =
+    List.intersperse (text " ") >> element
+
+
 notFound : String -> Model
 notFound error =
     { support_message = default.support_message
     , error_message =
-        span []
+        withSpacing (span [])
             [ i [ class "fa-solid fa-triangle-exclamation foo-term-yellow" ] []
-            , text " I could not find anything on this "
+            , text "I could not find anything on this"
             , i [ class "fa-solid fa-link" ] []
             , text error
-            , text " . If you think this is a mistake, please contact me."
+            , text ". If you think this is a mistake, please contact me."
             , i [ class "fa-solid fa-triangle-exclamation foo-term-yellow" ] []
             ]
     }
@@ -59,23 +61,19 @@ notFound error =
 default : Model
 default =
     { support_message =
-        span []
+        withSpacing (span [])
             [ a [ href "https://github.com/avinal" ] [ i [ class "fa-brands fa-github" ] [] ]
-            , text "&nbsp;&nbsp;&nbsp;"
             , a [ href "https://www.linked.com/in/avinal" ] [ i [ class "fa-brands fa-linkedin" ] [] ]
-            , text "&nbsp;&nbsp;&nbsp;"
             , a [ href "https://instagram.com/avinal.k" ] [ i [ class "fa-brands fa-instagram" ] [] ]
-            , text "&nbsp;&nbsp;&nbsp;"
-            , a [ href "" ] [ i [ class "fa-solid fa-calandar-days" ] [] ]
+            , a [ href "https://meet.avinal.space" ] [ i [ class "fa-solid fa-calandar-days" ] [] ]
             , a [ href "mailto:ripple+blog@avinal.space" ] [ i [ class "fa-solid fa-envelope" ] [] ]
-            , text "&nbsp;&nbsp;&nbsp;"
             , a [ href "https://avinal.space/terminal" ] [ i [ class "fa-solid fa-terminal" ] [] ]
             ]
     , error_message =
-        span []
-            [ text "I'm Avinal, and I work at Red Hat "
+        withSpacing (span [])
+            [ text "I'm Avinal, and I work at Red Hat"
             , i [ class "fa-brands fa-redhat foo-term-red" ] []
-            , text " as an Associate Software Engineer for Hybrid Cloud Engineering."
+            , text "as an Associate Software Engineer for Hybrid Cloud Engineering."
             ]
     }
 
